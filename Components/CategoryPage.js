f3308a2a579ee59cd18388886b733fda2141d954
ErikMyllymaki <erik.myl01@gmail.com>
2023-03-16 13:27:03 +0200

    category page

---
+++ Components/CategoryPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native';
+import Styles from './style/style';
+
+
+export default function CategoryPage() {
+  return (
+    <View style={Styles.container}>
+        <Text>categories</Text>
+    </View>
+  )
+}
