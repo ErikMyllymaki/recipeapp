b4b3c6635cccdf31e923e0768b4ecf2980aa15eb
ErikMyllymaki <erik.myl01@gmail.com>
2023-03-16 13:23:22 +0200

    reseptisivu

---
+++ Components/Recipe.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native';
+import Styles from './style/style';
+
+
+export default function Recipe() {
+  return (
+    <View style={Styles.container}>
+        <Text>recipe</Text>
+    </View>
+  )
+}
