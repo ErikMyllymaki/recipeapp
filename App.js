750e40b5b2472c4b056c74afb7973d41713a11dc
Jussi Kujala <jussijustus@gmail.com>
2023-03-16 14:50:03 +0200

    modified:   App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Header from './components/Header';
+import Header from './Components/Header';
 import Styles from './style/style';
 
 export default function App() {
