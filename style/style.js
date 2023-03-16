2770e41122b8f7cd40f49cce88e4f54aa6fa3031
ErikMyllymaki <erik.myl01@gmail.com>
2023-03-16 15:13:33 +0200

    centered pois

--- style/style.js
+++ style/style.js
@@ -4,8 +4,6 @@ export default StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
     },
 
     header: {
