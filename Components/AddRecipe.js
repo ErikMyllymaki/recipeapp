3493905abb0db47e713138393c720a6b43132c5e
ErikMyllymaki <erik.myl01@gmail.com>
2023-03-16 13:31:54 +0200

    AddRecipe added

---
+++ Components/AddRecipe.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native';
+import Styles from './style/style';
+
+
+export default function AddRecipe() {
+  return (
+    <View style={Styles.container}>
+        <Text>addrecipe</Text>
+    </View>
+  )
+}
