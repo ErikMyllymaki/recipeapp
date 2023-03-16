4cbeafcba921e49bff4b0bad3831bbe42173f1c5
ErikMyllymaki <erik.myl01@gmail.com>
2023-03-16 12:21:33 +0200

    Styles luotu

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Styles from './style/style';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={Styles.container}>
       <Text>Open up App.jss to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
