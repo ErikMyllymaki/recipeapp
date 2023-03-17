2a0f26468caed90363f71c5f7bb7cfe159771ae2
ErikMyllymaki <erik.myl01@gmail.com>
2023-03-17 16:15:30 +0200

    juu

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './components/Header';
 import Styles from './style/style';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from './components/Home';
+import CategoryPage from './components/CategoryPage';
+import AddRecipe from './components/AddRecipe';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={Styles.container}>
+    <NavigationContainer>
       <Header />
-      <Text>Open up App.jss to start working on your app!</Text>
-    </View>
+      <Tab.Navigator
+              screenOptions={{ headerShown: false }}
+        >
+        <Tab.Screen name="Home" component={Home} options={{ tabBarStyle: { display: 'none' } }}/>
+        <Tab.Screen name='Recipes' component={CategoryPage} />
+        <Tab.Screen name='Add recipes' component={AddRecipe} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
