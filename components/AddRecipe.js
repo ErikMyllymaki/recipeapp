343a0b88d07b027e8d38d517fc470e23a2933ee4
ErikMyllymaki <erik.myl01@gmail.com>
2023-03-23 14:07:26 +0200

    async storage jne

--- components/AddRecipe.js
+++ components/AddRecipe.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, Text } from 'react-native';
 import Styles from '../style/style';
 
+const STORAGE_KEY = "@recipe_Key";
 
 export default function AddRecipe() {
+
+  const [recipes, setRecipes] = useState([]);
+
+  const storeData = async (value) => {
+    try {
+      const jsonValue = JSON.stringify(value);
+      await AsyncStorage.setItem(STORAGE_KEY,jsonValue);
+    }catch (e) {
+      console.log(e)
+    }
+  }
+
   return (
     <View style={Styles.container}>
         <Text>addrecipe</Text>
