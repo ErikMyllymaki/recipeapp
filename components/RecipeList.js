0060affb0d3605700adf36c347fbe766db6ff444
ErikMyllymaki <erik.myl01@gmail.com>
2023-03-23 13:52:07 +0200

    recipelist

---
+++ components/RecipeList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native';
+import Styles from '../style/style';
+
+export default function RecipeList() {
+  return (
+    <View style={Styles.container}>
+        <Text>dmasodmaso</Text>
+    </View>
+  )
+}
