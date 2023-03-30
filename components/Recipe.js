baa6ed019bedf8e5cf121e284b73cf1da90e2168
ErikMyllymaki <erik.myl01@gmail.com>
2023-03-30 13:14:35 +0300

    alustava takaisin-nuoli reseptiin (kommenteissa koska ei toimi vielä oikein)

--- components/Recipe.js
+++ components/Recipe.js
@@ -1,14 +1,19 @@
 import React from 'react'
-import { View, Text } from 'react-native';
+import { View, Text, Pressable } from 'react-native';
 import Styles from '../style/style';
+// import { AntDesign } from '@expo/vector-icons';
 
-export default function Recipe({route}) {
+
+export default function Recipe({route, navigation}) {
 
   const { recipe } = route.params;
 
   return (
     <View style={Styles.container}>
-        <Text>{recipe.name}</Text>
-    </View>
+          {/* <Pressable onPress={() => navigation.navigate('RecipeList')}>
+          <AntDesign name='left' size={30} color='#4B702F'/>
+        </Pressable> */}
+        <Text style={Styles.pageHeader}>{recipe.name}</Text>
+      </View>
   )
 }
