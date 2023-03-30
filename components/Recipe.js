c22e07bb91642a791a5f1dfbb3618f97cd291e14
ErikMyllymaki <erik.myl01@gmail.com>
2023-03-30 12:24:24 +0300

    moi

--- components/Recipe.js
+++ components/Recipe.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { View, Text } from 'react-native';
 import Styles from '../style/style';
 
-export default function Recipe() {
+export default function Recipe({route}) {
   return (
     <View style={Styles.container}>
         <Text>recipie</Text>
