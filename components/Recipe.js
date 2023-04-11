88bba37f9baeee46028c2d8ff386ff6f7c735d64
Jennivis <n1vije00@students.oamk.fi>
2023-04-11 11:09:17 +0300

    Roskis-ikoni lisätty

--- components/Recipe.js
+++ components/Recipe.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Pressable, Image, ScrollView, Button } from 'react-native';
+import { View, Text, Pressable, Image, ScrollView, Button, TouchableWithoutFeedback } from 'react-native';
 import Styles from '../style/style';
 import { child, push, ref, remove, update, onValue } from 'firebase/database';
 import { db, RECIPES_REF } from '../firebase/config';
+import { EvilIcons } from '@expo/vector-icons';
 
 export default function Recipe({ route, navigation }) {
 
-  const { recipe } = route.params || {}; 
+  const { recipe } = route.params || {};
   const [recipeData, setRecipeData] = useState(null);
 
   useEffect(() => {
@@ -16,11 +17,11 @@ export default function Recipe({ route, navigation }) {
         setRecipeData(snapshot.val());
       });
     }
-  }, [recipe?.key]); 
+  }, [recipe?.key]);
 
   const removeRecipe = (recipeKey) => {
     const updates = {};
-    updates[`${RECIPES_REF}/${recipeKey}`] = null; 
+    updates[`${RECIPES_REF}/${recipeKey}`] = null;
     update(ref(db), updates);
   };
 
@@ -31,17 +32,20 @@ export default function Recipe({ route, navigation }) {
     <ScrollView>
       <View style={Styles.container}>
         <View style={Styles.recipeBackground}>
-        <View style={{ alignItems: 'center' }}>
-          <Image source={recipeImage} style={Styles.recipeImage} />
+          <View style={{ alignItems: 'center' }}>
+            <Image source={recipeImage} style={Styles.recipeImage} />
           </View>
           <View style={Styles.recipeInfo}>
             <Text style={Styles.pageHeader}>{recipeData?.recipeName}</Text>
             <Text style={Styles.recipeSubtitle}>Ingredients: {recipeData?.ingredients}</Text>
             <Text style={Styles.recipeSubtitle}>Instructions: {recipeData?.instructions}</Text>
-            <Button
-            title="Remove"
-            onPress={() => removeRecipe(recipe.key)}
-          />
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <TouchableWithoutFeedback onPress={() => removeRecipe(recipe.key)}>
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                  <EvilIcons name="trash" size={40} />
+                </View>
+              </TouchableWithoutFeedback>
+            </View>
           </View>
         </View>
       </View>
