9df1d9e35cb548aa914f0be056c82340fff956a7
JussiKujala <jussijustus@gmail.com>
2023-04-12 15:19:40 +0300

    modified:   components/Login.js

--- components/Login.js
+++ components/Login.js
@@ -1,7 +1,99 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { View, Text, TextInput, Pressable, Alert, Button } from 'react-native';
+import { signIn, resetPassword } from './Auth';
+import { onAuthStateChanged } from 'firebase/auth';
+import styles from '../style/style';
+import { auth } from '../firebase/config';
 
-export default function Login() {
+export default Login = ( { navigation } ) => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showForgotPw, setShowForgotPw] = useState(false);
+  const [emailForgotPw, setEmailForgotPw] = useState('');
+
+  const handlePress = () => {
+    if (!email) {
+      Alert.alert('Email is required.');
+    }
+    else if (!password) {
+      Alert.alert('Password is required.');
+    }
+    else {
+      signIn(email, password);
+      onAuthStateChanged(auth, async (user) => {
+        if (user) {
+          navigation.navigate('Todo', {userUid: user.uid});
+        }
+      });
+    }
+  };
+
+  const handlePressForgotPw = () => {
+    setShowForgotPw(!showForgotPw);
+  }
+
+  const handlePressResetPw = () => {
+    if (!emailForgotPw) {
+      Alert.alert('Email is required.');
+    }
+    else {
+      resetPassword(emailForgotPw);
+      setShowForgotPw(false);
+    }
+  }
+
   return (
-    <></>
-  )
+    <View>
+      <Text >Todos</Text>
+      <Text >Login to your account</Text>
+      <TextInput
+        placeholder="Enter your email*"
+        value={email}
+        onChangeText={(email) => setEmail(email)}
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+      <TextInput
+        placeholder="Enter your password*"
+        value={password}
+        onChangeText={(password) => setPassword(password)}
+        secureTextEntry={true}
+      />
+      <Pressable>
+        <Button 
+          title="Login"
+          onPress={handlePress} />
+      </Pressable>
+      <Text>Not having account yet?</Text>
+      <Pressable >
+        <Button
+          title="Register"
+          onPress={() => navigation.navigate('Register')} />
+      </Pressable>
+      <Pressable >
+        <Text 
+          onPress={handlePressForgotPw}>Forgot password</Text>
+      </Pressable>
+      { showForgotPw &&
+        <>
+          <TextInput
+            placeholder="Enter your email*"
+            value={emailForgotPw}
+            onChangeText={(emailForgotPw) => setEmailForgotPw(emailForgotPw)}
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+          <Pressable>
+            <Button
+              title="Reset password"
+              onPress={() => handlePressResetPw()} />
+          </Pressable>
+          <Text>
+            Be sure to check your spam folder after resetting!
+          </Text>
+        </>
+      }
+    </View>
+  );
 }
