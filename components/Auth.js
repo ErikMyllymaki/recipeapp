2f9b11e1a4d15e35fdbd8c0bb1175bf7f787b0e7
ErikMyllymaki <erik.myl01@gmail.com>
2023-04-12 14:46:43 +0300

    signUp lisätty to Auth.js

--- components/Auth.js
+++ components/Auth.js
@@ -5,3 +5,21 @@ import {
     singInWithEmailAndPassword,
     signOut } from 'firebase/auth';
     import { auth, db, USERS_REF } from '../firebase/config';
+import { async } from "@firebase/util";
+
+    export const signUp = async (nickname, email, password) => {
+        try {
+            await createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                set(ref(db, USERS_REF + userCredential.user.uid), {
+                    nickname: nickname,
+                    email: userCredential.user.email
+                });
+            })
+        }
+        catch (error) {
+            console.log("Registration failed. ", error.message);
+            Alert.alert("Registration failed. ", error.message);
+        }
+        
+    }
