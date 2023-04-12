ce705fe0d376a707adcdc0ddc22d346e3e84aa37
JussiKujala <jussijustus@gmail.com>
2023-04-12 14:45:48 +0300

    modified:   components/Auth.js

--- components/Auth.js
+++ components/Auth.js
@@ -3,5 +3,27 @@ import { ref, set } from 'firebase/database';
 import {
     createUserWithEmailAndPassword,
     singInWithEmailAndPassword,
-    signOut } from 'firebase/auth';
+    signOut, 
+    signInWithEmailAndPassword} from 'firebase/auth';
     import { auth, db, USERS_REF } from '../firebase/config';
+
+
+export const signIn = async (email, password) => {
+    try {
+        await signInWithEmailAndPassword(auth, email, password);
+    }
+    catch(error){
+        console.log("Login failed. ", error.message);
+        Alert.alert("Login failed. ", error.message);
+    };
+}
+
+export const logout = async () => {
+    try {
+        await signOut(auth);
+    }
+    catch(error) {
+        console.log("Logout error. ", error.message);
+        Alert.alert("Logout error. ", error.message);
+    };
+}
