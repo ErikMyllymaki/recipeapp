a51becfe6d4c003d2b8a2acd73f46e7c20918a43
JussiKujala <jussijustus@gmail.com>
2023-04-12 14:53:56 +0300

    modified:   components/Welcome.js

---
+++ components/Welcome.js
@@ -0,0 +1,21 @@
+import { View, Text, Button } from 'react-native';
+import Styles from '../style/style';
+
+
+export default Welcome = ({ navigation }) => {
+
+
+    return (
+        <View style={Styles.container}>
+            <Text>Welcome.js sivu</Text>
+            <Button 
+            title='Register'
+            onPress={() => navigation.navigate('Register')}
+            />
+            <Button
+            title='Login'
+            onPress={() => navigation.navigate('Login')}
+            />
+        </View>
+    )
+}
