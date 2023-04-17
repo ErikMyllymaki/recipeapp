ccbced72753584a5ffda3cbb16e9ae2f6aca7882
Jussi Kujala <jussijustus@gmail.com>
2023-04-17 10:15:58 +0300

    modified:   components/Header.js

--- components/Header.js
+++ components/Header.js
@@ -4,32 +4,34 @@ import { Text, View } from 'react-native';
 import { Image, Dimensions } from 'react-native';
 import { useState, useEffect } from 'react';
 import { auth } from '../firebase/config';
-import { db, RECIPES_REF, USERS_REF, FAVORITES_REF } from '../firebase/config';
-import { child, push, ref, remove, update, onValue, set, get } from 'firebase/database';
-
+import { db, USERS_REF } from '../firebase/config';
+import { onValue, ref } from 'firebase/database';
 
 export default function Header() {
+  const [userNickname, setUserNickname] = useState('');
 
-    const [userNickname, setUserNickname] = useState('');
-
-    useEffect(() => {
-      const user = auth.currentUser;
+  useEffect(() => {
+    const unsubscribeAuth = auth.onAuthStateChanged(user => {
       if (user) {
         const userRef = ref(db, `${USERS_REF}/${user.uid}/nickname`);
         onValue(userRef, snapshot => {
           setUserNickname(snapshot.val() || '');
         });
+      } else {
+        setUserNickname('');
       }
-    }, []);
+    });
 
-    const yourImage = require('../images/Logo.png');
-    const screenWidth = Dimensions.get('window').width;
+    return () => unsubscribeAuth();
+  }, []);
 
+  const yourImage = require('../images/Logo.png');
+  const screenWidth = Dimensions.get('window').width;
 
-    return (
-        <View style={Styles.header}>
-            <Image source={yourImage} style={{ resizeMode: 'contain', height: 75}}></Image>
-            <Text>{userNickname}</Text>
-        </View>
-    )
+  return (
+    <View style={Styles.header}>
+      <Image source={yourImage} style={{ resizeMode: 'contain', height: 75 }} />
+      <Text>{userNickname}</Text>
+    </View>
+  );
 }
