932b8d77cf54b40de005014b386478dfe89c0305
Jennivis <n1vije00@students.oamk.fi>
2023-04-17 11:39:53 +0300

    Tervetuloa-sivua tyylitelty

--- components/Welcome.js
+++ components/Welcome.js
@@ -1,14 +1,18 @@
-import { View, Text, Pressable } from 'react-native';
+import { View, Text, Pressable, ImageBackground, Dimensions } from 'react-native';
 import Styles from '../style/style';
 
 
 export default Welcome = ({ navigation }) => {
 
+    const backgroundImage = require('../images/backgroundImage.jpg');
+    const screenWidth = Dimensions.get('window').width;
+    const screenHeight = Dimensions.get('window').height;
 
 
     return (
         <View style={[Styles.container, {flex: 1, justifyContent: 'center', alignItems:'center'}]}>
-            <Text style={Styles.loginHeader}>Welcome to the RecipeHub!</Text>
+            <ImageBackground source={backgroundImage} style={{width: screenWidth, height: screenHeight, alignItems:'center', justifyContent: 'center', resizeMode: 'contain'}}>
+            {/* <Text style={Styles.loginHeader}>Welcome to the RecipeHub!</Text> */}
             <Pressable 
             onPress={() => navigation.navigate('Register')} 
             style={{ paddingVertical: 30 }}
@@ -17,6 +21,13 @@ export default Welcome = ({ navigation }) => {
             onPress={() => navigation.navigate('Login')}
             style={{ paddingVertical: 30 }}
             ><Text style={Styles.homeButton}>Login</Text></Pressable>
+            <Pressable
+            onPress={() => navigation.navigate('About')}
+            style={{ paddingVertical: 30 }}
+          >
+            <Text style={Styles.homeButton}>About recipe app</Text>
+          </Pressable>
+            </ImageBackground>
         </View>
     )
 }
