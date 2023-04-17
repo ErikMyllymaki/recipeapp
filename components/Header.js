2f253382065f050a88ca4d35c15a5d3454e48450
ErikMyllymaki <erik.myl01@gmail.com>
2023-04-17 09:57:16 +0300

    lol

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Styles from '../style/style';
 import { Text, View } from 'react-native';
 import { Image, Dimensions } from 'react-native';
+import { db, USERS_REF } from '../firebase/config';
+import { auth } from '../firebase/config';
 
 
 
 
 export default function Header() {
+
+    const [userKey, setUserKey] = useState('');
+    const [nickname, setNickname] = useState('');
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(user => {
+          if (user) {
+            const userRef = ref(db, USERS_REF + '/' + user.uid);
+            onValue(userRef, snapshot => {
+              const userData = snapshot.val();
+              if (userData) {
+                setUserKey(user.uid);
+                setNickname(userData.nickname); 
+              }
+            });
+          }
+        });
+      
+        return () => unsubscribe();
+      }, []);
 
     const yourImage = require('../images/Logo.png');
     const screenWidth = Dimensions.get('window').width;
@@ -15,6 +37,7 @@ export default function Header() {
     return (
         <View style={Styles.header}>
             <Image source={yourImage} style={{ resizeMode: 'contain', height: 75}}></Image>
+            <Text>{nickname}</Text>
         </View>
     )
 }
