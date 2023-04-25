555d7cf23dd39bba4fc3f6780fd690bc503d2910
Jennivis <n1vije00@students.oamk.fi>
2023-04-25 11:55:12 +0300

    tyylejä lisätty

--- style/style.js
+++ style/style.js
@@ -22,7 +22,7 @@ export default StyleSheet.create({
     fontStyle: 'italic'
   },
   userNickname: {
-    fontWeight: 'bold',
+    color: '#183007ff',
     fontStyle: 'italic'
   },
   // loginHeader: {
